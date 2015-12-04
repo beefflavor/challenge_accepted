441f9a5f29baa0580c58951c8944e823b217244c
josiah lewis mckinney <sparatan117@josiahs-MacBook-Pro.local>
2015-12-04 10:47:02 -0800

    added users and models

--- challenge/models.py
+++ challenge/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# class Profile(models.Model):
+#     username = models.CharField(max_length=20)
+
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    user = models.ForeignKey(Profile)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User)
