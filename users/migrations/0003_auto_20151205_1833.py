719ec98341b82d737fb5458fd3b9206dc9bacac5
josiah lewis mckinney <sparatan117@josiahs-MacBook-Pro.local>
2015-12-05 10:34:17 -0800

    added profile 2

---
+++ users/migrations/0003_auto_20151205_1833.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0002_remove_profile_ranking'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='profile',
+            name='profile_picture',
+            field=models.ImageField(blank=True, upload_to='media/profile_pictures', null=True),
+        ),
+    ]
